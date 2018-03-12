8c83b8a897c082035a4d1096f7e13690949fadf8
Philemon Eichin <phileicht@gmx.de>
2018-03-12 22:59:22 +0100

    Add Scale enum

---
+++ phirSOFT.MusicTheory/Scale.cs
@@ -0,0 +1,13 @@
+﻿namespace phirSOFT.MusicTheory
+{
+    public enum Scale
+    {
+        Ionian = 0,
+        Dorian = 2,
+        Phrygian = 4,
+        Lydian = 5,
+        Mixolydian = 7,
+        Aeolian = 9,
+        Locrian = 11
+    }
+}
