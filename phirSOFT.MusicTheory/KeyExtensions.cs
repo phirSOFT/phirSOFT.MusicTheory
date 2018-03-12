236745517ae76123c3a6dce25b66bc2af6d521c4
Philemon Eichin <phileicht@gmx.de>
2018-03-13 00:02:18 +0100

    Add KeyExtensions

---
+++ phirSOFT.MusicTheory/KeyExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace phirSOFT.MusicTheory
+{
+    public static class KeyExtensions
+    {
+        public static Key Major(this Key.Pitch pitch)
+        {
+            return Key.FromPitch(pitch, Scale.Ionian);
+        }
+
+        public static Key Minor(this Key.Pitch pitch)
+        {
+            return Key.FromPitch(pitch, Scale.Aeolian);
+        }
+    }
+}
