73b4b1adc8d891294b424f091ac1875e1ca81d42
Philemon Eichin <phileicht@gmx.de>
2018-03-12 12:01:11 +0100

    Add Interval struct

---
+++ phirSOFT.MusicTheory/Interval.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace phirSOFT.MusicTheory
+{
+
+    public struct Interval
+    {
+        private readonly int _semitones;
+
+        private Interval(int semitones)
+        {
+            _semitones = semitones;
+        }
+
+        public Interval FromSemitones(int semitones)
+        {
+            return new Interval(semitones);
+        }
+
+        public static Interval operator +(Interval left, Interval right)
+        {
+            return new Interval(left._semitones + right._semitones);
+        }
+
+        public static Interval operator -(Interval left, Interval right)
+        {
+            return new Interval(left._semitones - right._semitones);
+        }
+
+        public static Interval operator ++(Interval other)
+        {
+            return new Interval(other._semitones + 1);
+        }
+    }
+}
